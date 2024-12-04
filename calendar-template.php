bddaa593c3c2ee8892f7c933240c951717fdb618
Khilinski Valery <35501525+freelanse@users.noreply.github.com>
2024-12-04 21:24:28 +0300

    Create calendar-template.php

---
+++ calendar-template.php
@@ -0,0 +1,13 @@
+<?php
+/* Template Name: Calendar */
+get_header();
+?>
+
+<div id="calendar-controls">
+    <button id="prev-month">Предыдущий</button>
+    <span id="calendar-month"></span>
+    <button id="next-month">Следующий</button>
+</div>
+<div id="calendar-container"></div>
+
+<?php get_footer(); ?>
